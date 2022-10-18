cea860669c242e643641260281c09d01e3067f6e
Anabella La Padula <anabella.lapadula@gmail.com>
2022-10-18 15:03:33 -0300

    Segunda entrega

--- archivo.js
+++ archivo.js
@@ -1,15 +1,17 @@
-const ESPACIO= " " 
-let mensaje = "¿Estas listo? ¡Vamos a aprender! :)";
-alert(mensaje);
+/*//const ESPACIO= " " 
+//let mensaje = "¿Estas listo? ¡Vamos a aprender! :)";
+//alert(mensaje);
 
 let nombreDeUsuario = prompt("Ingresa tu nombre completo");
 let apellidoDeUsario = prompt("Ingresa tu Apellido");
 
 if((nombreDeUsuario !="")&& (apellidoDeUsario != "")){
-    alert("¡Bienvenido al sistema "+nombreDeUsuario+ ESPACIO+apellidoDeUsario+"!")
+    alert(`Bienvenido al sistema ${nombreDeUsuario} ${apellidoDeUsario}`)
 }else{
     alert("¡Debe ingresar nombre y apellido para continuar!" );
+
 }
+
 
 let  anioNacimiento = parseInt(prompt("Por favor ingresa el año de tu nacimiento"));
 if (anioNacimiento <= 2004){
@@ -17,9 +19,9 @@ if (anioNacimiento <= 2004){
 }else{
     alert(" Para poder ingresar debes ser mayor de 18 años")
 }
-
+ Primer entrega 
 for(let i = 1; i <=3 ; i++){
-    let palabra = prompt("Escribe como se dice CASA en ingles (letras minusculas)");
+    let palabra = prompt("Escribe como se dice CASA en ingles").toLocaleLowerCase();
     if ((palabra == "home")){
         alert("Excelente podes continuar con la siguiente palabra y seguir aprendiendo")
         break;
@@ -39,7 +41,7 @@ for(let i = 1; i <=3 ; i++){
         
         }
     for(let i = 1; i <=3 ; i++){
-            let palabra = prompt("Escribe como se dice MESA en ingles (letras minusculas)");
+            let palabra = prompt("Escribe como se dice MESA en ingles)").toLowerCase();
             if ((palabra == "table")){
                 alert("Excelente podes continuar con la siguiente palabra y seguir aprendiendo")
                 break;
@@ -49,7 +51,7 @@ for(let i = 1; i <=3 ; i++){
             
             }
     for(let i = 1; i <=3 ; i++){
-                let palabra = prompt("Escribe como se dice GATO en ingles (letras minusculas)");
+                let palabra = prompt("Escribe como se dice GATO en ingles").toLowerCase();
                 if ((palabra == "cat")){
                     alert("Excelente podes continuar con la siguiente palabra y seguir aprendiendo")
                     break;
@@ -59,7 +61,7 @@ for(let i = 1; i <=3 ; i++){
                 
                 }
     for(let i = 1; i <=3 ; i++){
-        let palabra = prompt("Escribe como se dice AUTO en ingles (letras minusculas)");
+        let palabra = prompt("Escribe como se dice AUTO en ingles").toLowerCase();
          if ((palabra == "car")){
             alert("Excelente podes continuar con la siguiente palabra y seguir aprendiendo")
             break;}else{
@@ -120,4 +122,74 @@ seleccionarOpcion ("eat", "drink", "COMER");
 seleccionarOpcion ( "drive","ride","CONDUCIR");
 seleccionarOpcion ( "run","walk","CORRER");
 seleccionarOpcion ( "cook","sleep","COCINAR");
+*/
 
+
+class producto{
+    constructor(codigo,  articulo, precio, descripcion, stock, marca){
+        this.codigo= codigo;
+        this.articulo= articulo;
+        this.precio= precio;
+        this.descripcion= descripcion;
+        this.stock= stock;
+        this.marca = marca;
+
+    }
+    restaStock(){
+        this.stock= this.stock - 1;
+        console.log(`el estock de ${this.articulo} ha sido actualizado`)
+    }
+}
+
+const producto0 = new producto(2410, "Mascara de Pestañas", 850, " color negro", 5, "mac");
+const producto1 = new producto(2200, "Base de maquillaje ", 1250, " Liquida", 20, "nars");
+const producto2 = new producto(2312, "Labial", 1200, "Color Rosa", 30, "maybelline");
+const producto3 = new producto(2528, "Labial", 1200, " Color Rojo", 15, "maybelline");
+const producto4 = new producto(2840, "Iluminador", 2000, "Glow", 5, "mac");
+const producto5 = new producto(2510, "Delineador", 600, "Color Negro", 8, "mac");
+
+
+//array
+
+const listaDeProductos = [producto0, producto1, producto2, producto3, producto4, producto5]
+
+
+
+listaDeProductos.forEach((elemento)=> console.log(elemento))
+
+
+let mensajePantalla = "Ingresa el código del producto que deseas. \n";
+for( maquillaje of listaDeProductos){
+   mensajePantalla += ` Código ${maquillaje.codigo}- ${maquillaje.articulo}: ${maquillaje.descripcion}, Valor $ ${maquillaje.precio}\n `
+}
+
+let elegir = prompt(mensajePantalla)
+
+const productoElegido = listaDeProductos.find((elemento) => elemento.codigo== elegir)
+if(productoElegido != undefined){ 
+alert( `Te vas a llevar ${productoElegido.articulo}  ${productoElegido.descripcion} a $ ${productoElegido.precio} `)
+}else{
+    alert(" Por el momento no tenemos ese producto  en stock")
+}
+
+
+
+let mensajePantallaDos = "Ingresa la marca del producto que deseas. \n";
+for( maquillaje of listaDeProductos){
+   mensajePantallaDos += ` Código ${maquillaje.codigo}- ${maquillaje.articulo}: ${maquillaje.descripcion}, Valor $ ${maquillaje.precio} ${maquillaje.marca}   \n `
+}
+let elegirPorMarca = prompt (mensajePantallaDos).toLowerCase();
+
+const buscarPorMarca = listaDeProductos.filter((articulo) => articulo.marca == elegirPorMarca );
+
+let mostrar = "Encontramos de la marca que escogiste:\n" 
+    for( const prod of buscarPorMarca){
+        mostrar += elegirPorMarca +` ${prod.articulo}  ${prod.precio} \n`
+    }
+    console.log( buscarPorMarca);
+    alert(mostrar);
+
+
+
+    const stock = listaDeProductos.reduce((cantidad,articulo)=>cantidad + articulo.stock, 0)
+    console.log("total de productos en stock"+stock) 
